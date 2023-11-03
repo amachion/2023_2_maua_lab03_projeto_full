ae12a3510ded8a4a247e079549ae18f25b4696eb
Andreia Cristina Grisolio Machion <andreia.machion@maua.br>
2023-11-03 11:03:04 -0300

    até código 2.12.1

--- front/js/script.js
+++ front/js/script.js
@@ -6,5 +6,14 @@ async function obterFilmes () {
     //console.log ("teste 1 2 3 ");
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`;
     const filmes = (await axios.get(URLcompleta)).data;
-    console.log (filmes);
+    //console.log (filmes);
+    let tabela = document.querySelector('.filmes');
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0];
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0);
+        let celulaTitulo = linha.insertCell(0);
+        let celulaSinopse = linha.insertCell(1);
+        celulaTitulo.innerHTML = filme.titulo;
+        celulaSinopse.innerHTML = filme.sinopse;
+    }
 }
